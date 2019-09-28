11bc9984c762852704ff60a8e1700bfed52c9b21
asemerenko <asemerenko@priocom.com>
2019-09-28 14:14:51 +0300

    first test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,6 +22,17 @@ public class CalculatorTest {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\projects\\autotestcourse2019\\src\\main\\resources\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://automationpractice.com/");
+        driver.findElement(By.id("search_query_top")).sendKeys("Bloose");
+        driver.findElement(By.name("submit_search")).click();
+        driver.findElement(By.id("list")).click();
+        driver.findElement(By.xpath("//a[@title='Add to cart']")).click();
+        driver.findElement(By.xpath("//*[@title='Proceed to checkout']")).click();
+        String value = driver.findElement(By.id("total_price")).getText();
+
+        Assert.assertEquals("Two elements NOT equa","29.00",value);
+
+        driver.close();
     }
 
     @Test

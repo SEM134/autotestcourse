5db1eef06e65b95a2eed9ae9bbbdd9b666370e5a
asemerenko <asemerenko@priocom.com>
2019-09-28 15:02:23 +0300

    refactoring global locators add

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -10,13 +10,17 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
     WebDriver driver;
+    final String expectedTotalPrice = "$29.00";
+    private String buttonAddToCart = "//a[@title='Add to cart']";
+    private static final String URL = "http://automationpractice.com/";
+    private String searchInput = "search_query_top";
 
     @Before
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\projects\\autotestcourse2019\\src\\main\\resources\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.navigate().to("http://automationpractice.com/");
+        driver.navigate().to(URL);
     }
 
     @After
@@ -26,14 +30,16 @@ public class FirstTest {
 
     @Test
     public void testToCart() {
-        driver.findElement(By.id("search_query_top")).sendKeys("Bloose");
+
+        driver.findElement(By.id(searchInput)).sendKeys("Bloose");
         driver.findElement(By.name("submit_search")).click();
         driver.findElement(By.id("list")).click();
-        driver.findElement(By.xpath("//a[@title='Add to cart']")).click();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);/*если в течении указаного времени элемент на будет найден, то будет exeption */
+        driver.findElement(By.xpath(this.buttonAddToCart)).click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);/*если в течении указаного времени элемент на будет найден, то будет exeption */
         driver.findElement(By.xpath("//*[@title='Proceed to checkout']")).click();
-        String value = driver.findElement(By.id("total_price")).getText();
+        String actualTotalPrice = driver.findElement(By.id("total_price")).getText();
 
-        Assert.assertEquals("Two elements NOT equal", "$29.00", value);
+        Assert.assertEquals("Two elements NOT equal", this.expectedTotalPrice, actualTotalPrice);
     }
+
 }

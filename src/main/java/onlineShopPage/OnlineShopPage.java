af5163c42c848b7c9139fcc94c8bb0ce69a6ae89
asemerenko <asemerenko@priocom.com>
2019-10-12 14:05:10 +0300

    refactoring test By pageObject

---
+++ src/main/java/onlineShopPage/OnlineShopPage.java
@@ -0,0 +1,65 @@
+package onlineShopPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class OnlineShopPage {
+    WebDriver driver;
+
+    private String buttonAddToCart = "//a[@title='Add to cart']";
+    private String searchInput = "search_query_top";
+
+    public OnlineShopPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(By.xpath("//div[@id='create_account_error']")).getText();
+    }
+
+    public void clickButtonCreateAccount() {
+        driver.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
+    }
+
+    public void enterIntoEmailField(String s) {
+        driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(s);
+    }
+
+    public void clickSignIn() {
+        driver.findElement(By.xpath("//a[@class='login']")).click();
+    }
+
+
+    public void enterIntoSearchField(String item) {
+        driver.findElement(By.id(searchInput)).sendKeys(item);
+    }
+
+    public void clickSearchButton() {
+        driver.findElement(By.name("submit_search")).click();
+    }
+
+    public void clickListView() {
+        driver.findElement(By.id("list")).click();
+    }
+
+    public void clickButtonAddToCart() {
+        driver.findElement(By.xpath(this.buttonAddToCart)).click();
+    }
+
+    public void clickProccedToCheckoutButton() {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@title='Proceed to checkout']")))).click();
+    }
+
+    public void waitEmailField() {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated((By.id("email_create"))));
+    }
+
+    public void waitErrorMessage() {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@id='create_account_error']"))));
+    }
+}
--- src/test/java/NGOnlineShopTest.java
+++ src/test/java/NGOnlineShopTest.java
@@ -1,4 +1,5 @@
 
+import onlineShopPage.OnlineShopPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.interactions.Actions;
@@ -10,67 +11,11 @@ import org.testng.annotations.Test;
 
 public class NGOnlineShopTest extends BaseTest {
 
-    final String expectedTotalPrice = "$29.00";
-    private String buttonAddToCart = "//a[@title='Add to cart']";
-    private String searchInput = "search_query_top";
-
-    @Test
-    public void testToCart() {
-        enterIntoSearchField("Bloose");
-        clickSearchButton();
-        clickListView();
-        clickButtonAddToCart();
-        clickProccedToCheckoutButton();
-        String actualTotalPrice = driver.findElement(By.id("total_price")).getText();
-
-        Assert.assertEquals("Two elements NOT equal", this.expectedTotalPrice, actualTotalPrice);
-    }
-
-    @Test
-    public void testToCartWithPluss() {
-        String expectedTotalPrice = "$56.00";
-        driver.findElement(By.id(searchInput)).sendKeys("Bloose");
-        driver.findElement(By.name("submit_search")).click();
-        clickListView();
-        driver.findElement(By.xpath("//*[@id='center_column']/ul/li/div/div/div[3]/div/div[2]/a[2]")).click();
-        driver.findElement(By.xpath("//*[@id='quantity_wanted_p']/a[2]")).click();
-        driver.findElement(By.xpath("//*[@id='add_to_cart']/button")).click();
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@title='Proceed to checkout']")))).click();
-        String actualTotalPrice = driver.findElement(By.id("total_price")).getText();
-
-        Assert.assertEquals("Two elements NOT equal", expectedTotalPrice, actualTotalPrice);
-    }
-
-    @Test
-    public void testToCartWithPlussAndDELETE() {
-        String expectedTotalPrice = "Your shopping cart is empty.";
-        driver.findElement(By.id(searchInput)).sendKeys("Bloose");
-        driver.findElement(By.name("submit_search")).click();
-        clickListView();
-
-        driver.findElement(By.xpath("//*[@id='center_column']/ul/li/div/div/div[3]/div/div[2]/a[2]")).click();
-        driver.findElement(By.xpath("//*[@id='quantity_wanted_p']/a[2]")).click();
-        driver.findElement(By.xpath("//*[@id='add_to_cart']/button")).click();
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(driver.
-                findElement(By.xpath("//*[@title='Proceed to checkout']")))).click();
-
-        driver.findElement(By.xpath("//*[@id='2_7_0_0']")).click();
-
-        String actualTotalResult = "";
-        try {
-            actualTotalResult = webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id='center_column']/p")))).getText();
-        } catch (StaleElementReferenceException e) {
-            actualTotalResult = webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id='center_column']/p")))).getText();
-        }
-
-
-        Assert.assertEquals("Two elements NOT equal", expectedTotalPrice, actualTotalResult);
-    }
+    OnlineShopPage page;
 
     @Test
     public void verifyAddItemToCartWithActins() {
+        page = new OnlineShopPage(driver);
         Actions actions = new Actions(driver);
         actions.moveToElement(driver.findElement(By.xpath("//a[@title='Women']"))).build().perform();
 
@@ -79,24 +24,14 @@ public class NGOnlineShopTest extends BaseTest {
 
     @Test(dataProvider = "create account")
     public void verifyCreateAccount(String email, String massage) {
-//        String mes1 = "Invalid email address.";
-//        String mes2 = "An account using this email address has already been registered. Please enter a valid password or request a new one.";
-        clickSignIn();
-        waitEmailField();
-        enterIntoEmailField(email);
-        clickButtonCreateAccount();
-        waitErrorMessage();
-        Assert.assertEquals(getErrorMessage(), massage);
-    }
+        page = new OnlineShopPage(driver);
 
-    private void waitEmailField() {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated((By.id("email_create"))));
-    }
-
-    private void waitErrorMessage() {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@id='create_account_error']"))));
+        page.clickSignIn();
+        page.waitEmailField();
+        page.enterIntoEmailField(email);
+        page.clickButtonCreateAccount();
+        page.waitErrorMessage();
+        Assert.assertEquals(page.getErrorMessage(), massage);
     }
 
     @DataProvider(name = "create account")
@@ -107,41 +42,5 @@ public class NGOnlineShopTest extends BaseTest {
         };
     }
 
-    private String getErrorMessage() {
-        return driver.findElement(By.xpath("//div[@id='create_account_error']")).getText();
-    }
 
-    private void clickButtonCreateAccount() {
-        driver.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
-    }
-
-    private void enterIntoEmailField(String s) {
-        driver.findElement(By.xpath("//input[@id='email_create']")).sendKeys(s);
-    }
-
-    private void clickSignIn() {
-        driver.findElement(By.xpath("//a[@class='login']")).click();
-    }
-
-
-    private void enterIntoSearchField(String item) {
-        driver.findElement(By.id(searchInput)).sendKeys(item);
-    }
-
-    private void clickSearchButton() {
-        driver.findElement(By.name("submit_search")).click();
-    }
-
-    private void clickListView() {
-        driver.findElement(By.id("list")).click();
-    }
-
-    private void clickButtonAddToCart() {
-        driver.findElement(By.xpath(this.buttonAddToCart)).click();
-    }
-
-    private void clickProccedToCheckoutButton() {
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@title='Proceed to checkout']")))).click();
-    }
 }
